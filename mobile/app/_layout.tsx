b24ff89e5558fa2f5c919a74a41e83d68e9eca59
kingofdead6 <ny_kahlouche@esi.dz>
2025-08-17 20:00:33 +0100

    Added new features

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,15 +1,26 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import "../global.css";
 import Hero from '@/components/Hero';
 import Lives from '@/components/Lives';
 
-const Stack = createStackNavigator();
-const App = () => (
-  <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Hero" component={Hero} />
-      <Stack.Screen name="Lives" component={Lives} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+const Stack = createNativeStackNavigator();
+
+export default function RootLayout() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Hero">
+        <Stack.Screen 
+          name="Hero" 
+          component={Hero} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="Lives" 
+          component={Lives} 
+          options={{ headerShown: false }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
